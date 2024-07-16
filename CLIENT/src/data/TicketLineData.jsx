3c8c430b929f08caccf3ce7fad33c55e50098814
kamoteroy <kamoteroy6@gmail.com>
2024-07-17 00:06:01 +0800

    asdasda

--- CLIENT/src/data/TicketLineData.jsx
+++
@@ -1,9 +0,0 @@
-const columns = [
-  { name: "ID", uid: "Id", sortable: true },
-  { name: "TICKET ID ", uid: "TicketId", sortable: true },
-  { name: "DATE CALLED", uid: "DateCalled", sortable: true },
-  { name: "DATE FINISHED", uid: "DateFinished" },
-  { name: "ACTION", uid: "Action" },
-];
-
-export { columns };
