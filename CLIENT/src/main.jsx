da14440223529a2039e8a0397fb83af1bd350aa8
kamoteroy <kamoteroy6@gmail.com>
2024-07-22 21:24:39 +0800

    gggggggggggg

--- CLIENT/src/main.jsx
+++ CLIENT/src/main.jsx
@@ -3,17 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { NextUIProvider } from "@nextui-org/react";
 import "./styles/globals.css";
-//import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-//import userReducer from "./routes/userLogged.jsx";
 import { PersistGate } from "redux-persist/integration/react";
 import store, { Persistor } from "./routes/store.js";
-
-/*const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});*/
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
